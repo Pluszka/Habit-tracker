1925a5f72996d98898ea15b8835cafad6e3e4607
Pliszka <karoliniakkjk@gmail.com>
2022-02-14 22:52:09 +0100

    Created new user

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import requests
+import os
 
+USERNAME = 'pluszka'
+
+PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
+
+user_params = {
+    'token': os.environ.get('PIXELA_TOKEN'),
+    'username': USERNAME,
+    'agreeTermsOfService': 'yes',
+    'notMinor': 'yes',
+}
+#..............................CREATING USER..................................
+# response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# print(response.text)
+
+GRAPH_ENDPOINT =
