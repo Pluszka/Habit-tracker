eeb9120b897a235f7f29ee23d46e0507a191bda9
Pliszka <karoliniakkjk@gmail.com>
2022-02-14 23:07:48 +0100

    created new graph

--- main.py
+++ main.py
@@ -15,4 +15,20 @@ user_params = {
 # response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
 # print(response.text)
 
-GRAPH_ENDPOINT =
+GRAPH_ENDPOINT = f'{PIXELA_ENDPOINT}/{USERNAME}/graphs'
+
+graph_config = {
+    'id': 'comicg1',
+    'name': 'Working at my comic book',
+    'unit': 'hours',
+    'type': 'float',
+    'color': 'ichou'
+
+}
+
+headers = {
+    'X-USER-TOKEN': os.environ.get('PIXELA_TOKEN')
+}
+
+response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+print(response.text)
